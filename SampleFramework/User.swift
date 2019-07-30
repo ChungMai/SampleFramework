71d48ba8fff1237416bcd87c262382e6c8ab73c9
Chungmh <chungmh@vng.com.vn>
2019-07-30 16:31:21 +0700

    remove test

---
+++ SampleFramework/User.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SampleFramework
+//
+//  Created by ChungMai on 7/30/19.
+//  Copyright © 2019 ChungMai. All rights reserved.
+//
+
+import Foundation
+
+public class User {
+    var name: String?
+    var age: Int?
+}
